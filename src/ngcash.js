ae05c158eae71db863f59cd2b82964d06fb2c429
Caio Braga <caioaugustobrg@gmail.com>
2023-02-26 02:10:02 -0300

    identation

--- src/ngcash.js
+++ src/ngcash.js
@@ -1,85 +1,85 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = process.env.PORT || 3030;
-const path = require("path")
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const { eAdmin } = require("./middleware/auth.js");
-require("dotenv").config();
-const createUser = require("./middleware/createUser");
-const { PrismaClient } = require("@prisma/client");
+const path = require('path');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { eAdmin } = require('./middleware/auth.js');
+require('dotenv').config();
+const createUser = require('./middleware/createUser');
+const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
-const bodyParser = require('body-parser')
-const cors = require('cors')
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended: false}))
-app.use(bodyParser.json())
+app.use(express.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 // use `prisma` in your application to read and write data in your DB
 
 
 
-app.get("/", eAdmin, async (req, res) => {
-  return res.json({
-    erro: false,
-    mensagem: "Token autorizado!",
-  });
+app.get('/', eAdmin, async (req, res) => {
+	return res.json({
+		erro: false,
+		mensagem: 'Token autorizado!',
+	});
 });
 
-app.post("/register", createUser);
+app.post('/register', createUser);
 
-app.post("/login", async (req, res) => {
-  app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
-  console.log(req.body);
-  const {  password, email } = req.body;
-  const hashPassword = await bcrypt.hash(password, 8);
-  let token = jwt.sign({ id: 1 }, "AS3O20A193KS39DJANVN81937G", {
-    expiresIn: "7d",
-  });
-  try {
-    const user = await prisma.user.findUnique({
-      where: {
-        email: email 
+app.post('/login', async (req, res) => {
+	app.use(function(req, res, next) {
+		res.header('Access-Control-Allow-Origin', '*');
+		res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+		next();
+	});
+	console.log(req.body);
+	const {  password, email } = req.body;
+	const hashPassword = await bcrypt.hash(password, 8);
+	let token = jwt.sign({ id: 1 }, 'AS3O20A193KS39DJANVN81937G', {
+		expiresIn: '7d',
+	});
+	try {
+		const user = await prisma.user.findUnique({
+			where: {
+				email: email 
         
-      },
-    });
-    if (user === null) {
-      return res.status(400).json({
-        erro: true,
-        mensagem: "Erro: Email ou senha incorreto(s)!",
-      });
-    }
+			},
+		});
+		if (user === null) {
+			return res.status(400).json({
+				erro: true,
+				mensagem: 'Erro: Email ou senha incorreto(s)!',
+			});
+		}
 
-    const passwordMatch = await bcrypt.compare(password, user.password);
-    if (!passwordMatch) {
-      return res.status(400).json({
-        erro: true,
-        mensagem: "Erro: Usuário ou senha incorreto(s)!",
-      });
-    }
-    return res.status(200).json({
-      erro: false,
-      mensagem: "Login efetuado com sucesso!",
-      token: token,
-    });
-  } catch (error) {
-    return res.status(400).json({
-      erro: true,
-      mensagem: `Erro: " + ${error}`,
-    });
-  }
+		const passwordMatch = await bcrypt.compare(password, user.password);
+		if (!passwordMatch) {
+			return res.status(400).json({
+				erro: true,
+				mensagem: 'Erro: Usuário ou senha incorreto(s)!',
+			});
+		}
+		return res.status(200).json({
+			erro: false,
+			mensagem: 'Login efetuado com sucesso!',
+			token: token,
+		});
+	} catch (error) {
+		return res.status(400).json({
+			erro: true,
+			mensagem: `Erro: " + ${error}`,
+		});
+	}
 });
 
 app.listen(port, () =>
-  console.log(
-    `Express started on http://localhost:${port}; ` +
-      `press CRTL + C to terminate.`
-  )
+	console.log(
+		`Express started on http://localhost:${port}; ` +
+      'press CRTL + C to terminate.'
+	)
 );
