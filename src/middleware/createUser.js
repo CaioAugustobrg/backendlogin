0a0768e235206399b8b3dca1229ad1706715e8ad
Caio Braga <caioaugustobrg@gmail.com>
2023-02-26 02:13:03 -0300

    identation

--- src/middleware/createUser.js
+++ src/middleware/createUser.js
@@ -1,81 +1,80 @@
-const { PrismaClient } = require("@prisma/client");
+const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
-const bcrypt = require("bcryptjs");
-const nodemailer = require('nodemailer');
-require("dotenv").config();
-const sgMail = require('@sendgrid/mail')
+const bcrypt = require('bcryptjs');
+require('dotenv').config();
+const sgMail = require('@sendgrid/mail');
 
 module.exports = async (req, res) => {
-  try {
-    const { username, password, email } = req.body;
-    const hashPassword = await bcrypt.hash(req.body.password, 8);
-    const userExits = await prisma.user.findUnique({
-      where: { username },
-    });
+	try {
+		const { username, email } = req.body;
+		const hashPassword = await bcrypt.hash(req.body.password, 8);
+		const userExits = await prisma.user.findUnique({
+			where: { username },
+		});
 
-    if (userExits) {
-      return res.status(400).json({
-        erro: true,
-        mensagem: "Username em uso por outro usuário!",
-      });
-    }
+		if (userExits) {
+			return res.status(400).json({
+				erro: true,
+				mensagem: 'Username em uso por outro usuário!',
+			});
+		}
 
-    const emailExits = await prisma.user.findUnique({
-      where: { email },
-    });
+		const emailExits = await prisma.user.findUnique({
+			where: { email },
+		});
 
-    if (emailExits) {
-      return res.status(400).json({
-        erro: true,
-        mensagem: "Email em uso por outro usuário!",
-      });
-    }
+		if (emailExits) {
+			return res.status(400).json({
+				erro: true,
+				mensagem: 'Email em uso por outro usuário!',
+			});
+		}
 
-    const user = await prisma.user.create({
-      data: {
-        username: username,
+		const user = await prisma.user.create({
+			data: {
+				username: username,
 
-        email: email,
+				email: email,
 
-        password: hashPassword,
+				password: hashPassword,
 
-        createdAt: new Date()
-      },
-    });
+				createdAt: new Date()
+			},
+		});
 
     
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-    const msg = {
-      to: `${email}`,
-      from: 'caioaugustobrg@gmail.com',
-      subject: 'Obrigado por usar meu app',
-      text: `Eai, ${username}. Esse é um email automático enviado para o email que você cadastrou na minha aplicação. Você pode encontrar o algoritmo que faz isso aqui: https://github.com/CaioAugustobrg/backendlogin2/blob/main/src/middleware/createUser.js. Mais uma vez: muito obrigado por testar minha aplicação. Fale comigo: https://www.linkedin.com/in/caioaugustobrg/`,
-      html: `<strong>${username}</strong>`,
-    }
-    sgMail
-      .send(msg)
-      .then(() => {
-        console.log('Email sent')
-      })
-      .catch((error) => {
-        console.error(error)
-      })
+		sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+		const msg = {
+			to: `${email}`,
+			from: 'caioaugustobrg@gmail.com',
+			subject: 'Obrigado por usar meu app',
+			text: `Eai, ${username}. Esse é um email automático enviado para o email que você cadastrou na minha aplicação. Você pode encontrar o algoritmo que faz isso aqui: https://github.com/CaioAugustobrg/backendlogin2/blob/main/src/middleware/createUser.js. Mais uma vez: muito obrigado por testar minha aplicação. Fale comigo: https://www.linkedin.com/in/caioaugustobrg/`,
+			html: `<strong>${username}</strong>`,
+		};
+		sgMail
+			.send(msg)
+			.then(() => {
+				console.log('Email sent');
+			})
+			.catch((error) => {
+				console.error(error);
+			});
 
-    return res.status(200).json({
-      user,
-      erro: false,
-      mensagem: "Usuário cadastrado com sucesso!",
-      email: "Enviado"
-    });
-
-
+		return res.status(200).json({
+			user,
+			erro: false,
+			mensagem: 'Usuário cadastrado com sucesso!',
+			email: 'Enviado'
+		});
 
 
 
-  } catch (error) {
-    return res.status(400).json({
-      Erro: true,
-      mensagem: "Erro: " + error,
-    });
-  }
+
+
+	} catch (error) {
+		return res.status(400).json({
+			Erro: true,
+			mensagem: 'Erro: ' + error,
+		});
+	}
 };
