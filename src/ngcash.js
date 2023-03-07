175bd8e21172609e13ddf930d522d26308bd648b
Caio Braga <caioaugustobrg@gmail.com>
2023-03-06 23:01:42 -0300

    :sparkles: feat: added username as an obrigatory input to make login

--- src/ngcash.js
+++ src/ngcash.js
@@ -16,11 +16,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(bodyParser.json());
-
 app.use(express.static(path.join(__dirname, 'public')));
 // use `prisma` in your application to read and write data in your DB
-
-
 
 app.get('/', eAdmin, async (req, res) => {
 	return res.json({
@@ -31,44 +28,54 @@ app.get('/', eAdmin, async (req, res) => {
 
 app.post('/register', createUser);
 
+app.get('/Register', async (req,res) => {
+	res.render('/Register');
+});
+
 app.post('/login', async (req, res) => {
 	app.use(function(req, res, next) {
 		res.header('Access-Control-Allow-Origin', '*');
 		res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 		next();
 	});
+
 	console.log(req.body);
-	const {  password, email } = req.body;
+	const {  username, password, email } = req.body;
 	const hashPassword = await bcrypt.hash(password, 8);
 	let token = jwt.sign({ id: 1 }, 'AS3O20A193KS39DJANVN81937G', {
-		expiresIn: '7d',
+		expiresIn: '24h',
 	});
+
 	try {
 		const user = await prisma.user.findUnique({
 			where: {
-				email: email 
+				email: email,
+				username: username
         
 			},
 		});
-		if (user === null) {
-			return res.status(400).json({
+
+		if (user.email === null || user.username === null) {
+			return res.status(404).json({
 				erro: true,
-				mensagem: 'Erro: Email ou senha incorreto(s)!',
+				mensagem: 'Erro: Email, usuário ou senha incorreto(s)!',
 			});
 		}
 
 		const passwordMatch = await bcrypt.compare(password, user.password);
 		if (!passwordMatch) {
-			return res.status(400).json({
+			return res.status(404).json({
 				erro: true,
 				mensagem: 'Erro: Usuário ou senha incorreto(s)!',
 			});
 		}
+		
 		return res.status(200).json({
 			erro: false,
 			mensagem: 'Login efetuado com sucesso!',
 			token: token,
 		});
+
 	} catch (error) {
 		return res.status(400).json({
 			erro: true,
