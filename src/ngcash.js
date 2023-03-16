f81eff76061624fc4bed9ee553c37cff90d2b97e
Caio Braga <caioaugustobrg@gmail.com>
2023-03-16 18:00:10 -0300

    :recycle: refactor: routes moved to 'src/routes/routes.js'

--- src/ngcash.js
+++ src/ngcash.js
@@ -2,15 +2,9 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3030;
 const path = require('path');
-const { eAdmin } = require('./middleware/auth.js');
 require('dotenv').config();
-const createUser = require('./controllers/createUser');
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const userLogin = require('./controllers/userLogin.js');
-const transactionsRegister = require('./middleware/transactionsRegister.js');
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(bodyParser.json());
@@ -20,12 +14,7 @@ const router = require('../src/routes/routes');
 
 app.use(router);
 
-app.use('/transactions/:id', transactionsRegister ,router);
 
-app.use('/register',createUser , router);
-
-
-app.use('/login', userLogin, router);
 
 app.listen(port, () =>
 	console.log(
