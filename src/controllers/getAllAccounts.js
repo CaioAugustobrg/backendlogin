1dc039da475f1a30c914cc158e4e6ba027698c6d
Caio Braga <caioaugustobrg@gmail.com>
2023-03-16 12:35:40 -0300

    :bug: fix: corrected the prisma.acount.findMany and added a return

--- src/controllers/getAllAccounts.js
+++ src/controllers/getAllAccounts.js
@@ -3,16 +3,15 @@ const prisma = new PrismaClient();
 
 module.exports = async (req,res) => {
 	try {
-		const getAccounts = await prisma.account.findMany({
-			select: {accountId: true}
-		});
+		const getAccounts = await prisma.account.findMany({});
 		
-		if (getAccounts == null) {
+		if (!getAccounts) {
 			return res.status(400).json({
 				erro: true,
 				msg: 'getAccounts has a null value'
 			});
 		}
+		return res.json({getAccounts});
 	} catch (error) {
 		console.log(error);
 		return res.status(400).json({
