34b3b92227d2814cab4259e06c8973d186b63ebc
Caio Braga <caioaugustobrg@gmail.com>
2023-03-21 16:30:51 -0300

    :recycle: refactor: better way to do cors

--- src/ngcash.js
+++ src/ngcash.js
@@ -5,20 +5,30 @@ const path = require('path');
 require('dotenv').config();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(bodyParser.json());
+app.use(cookieParser());
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,PATCH,POST,DELETE,OPTIONS');
+	next();
+});
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors());
+app.use(cors({
+	credentials: true,
+	origin: true
+}));
 const router = require('../src/routes/routes');
 
 app.use(router);
 
-
-
 app.listen(port, () =>
 	console.log(
-		`Express started on 127.0.0.1:${port}; ` +
+		`Express started on http://localhost:${port}; ` +
       'press CRTL + C to terminate.'
 	)
 );
