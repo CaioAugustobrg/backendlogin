acea19238315c7a8f888581ce4740a87a9ed7323
Caio Braga <caioaugustobrg@gmail.com>
2023-04-02 17:57:22 -0300

    :bug: fix: fixed prisma search in db with id in req.params

--- src/controllers/getAllAccounts.js
+++ src/controllers/getAllAccounts.js
@@ -3,12 +3,19 @@ const prisma = new PrismaClient();
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const bodyParser = require('body-parser');
 app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json());
 
 module.exports = async (req,res) => {
 	let getAccounts;
+	const {user_id} = await req.params;
 	try {
-		getAccounts = await prisma.account.findMany({});
+		getAccounts = await prisma.account.findFirst({
+			where: {id: user_id},
+			select: {balance: true}
+		});
 		
 		if (!getAccounts) {
 			return res.status(400).json({
@@ -26,6 +33,6 @@ module.exports = async (req,res) => {
 	}
 	console.log(getAccounts);
 
-	return res.json(...getAccounts);
+	return res.json(getAccounts);
 };
 	
