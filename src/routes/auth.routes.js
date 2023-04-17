3188baa314a40f8c3cedb649bce4cb66051c997c
Caio Braga <caioaugustobrg@gmail.com>
2023-04-17 00:43:10 -0300

    :sparkles: feat: validators to routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,10 +1,12 @@
 const createUser = require('../controllers/createUser');
 const userLogin = require('../controllers/userLogin');
 const express = require('express');
+const bodyValidation = require('../utils/bodyValidation');
 const AuthRouter = express.Router();
+const {loginSchema, registrationSchema} = require('./validators/user.validator');
 
-AuthRouter.post('/register', createUser);
+AuthRouter.post('/register',[bodyValidation(registrationSchema)], createUser);
 
-AuthRouter.post('/login', userLogin);
+AuthRouter.post('/login', [bodyValidation(loginSchema)], userLogin);
 
 module.exports = AuthRouter;
