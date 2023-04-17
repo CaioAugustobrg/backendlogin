3871ec7b4485438eb0650c598b034d574bae86a6
Caio Braga <caioaugustobrg@gmail.com>
2023-04-17 00:49:02 -0300

    :sparkles: feat: imported others routes files

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,20 +1,16 @@
 const express = require('express');
-const createUser = require('../controllers/createUser');
-const userLogin = require('../controllers/userLogin');
 const { eAdmin } = require('../middleware/auth');
-const transactionsRegister = require('../controllers/transactionsRegister');
 const getAllAccounts = require('../controllers/getAllAccounts');
 const getAllUsers = require('../controllers/getAllUsers');
+const AuthRouter = require('./auth.routes');
+const transactionsRouter = require('./transactions.routes');
 const router = express.Router();
 
-router.put('/transactions/:debitedAccountId', eAdmin, transactionsRegister);
-
-router.post('/register', createUser);
-
-router.post('/login', userLogin);
 
 router.get('/getAllAccounts/:id',eAdmin, getAllAccounts);
 
+router.use('/auth', AuthRouter);
+router.use('/', transactionsRouter);
 router.get('/getAllUsers', eAdmin, getAllUsers);
 
 module.exports = router;
