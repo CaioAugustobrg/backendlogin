3adce8b42042c6f1efcf2d746694a7dab8292ab1
Caio Braga <caioaugustobrg@gmail.com>
2024-01-10 00:08:54 -0300

    :boom:  removed these route files because i decided to move to graphql

--- src/routes/transactions.routes.ts
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { eAdmin } = require('../middleware/auth');
-const transactionsRouter = express.Router();
-const transactionsRegister = require('../controllers/transactionsRegister');
-
-
-transactionsRouter.put('/transactions/:debitedAccountId', eAdmin, transactionsRegister);
-
-module.exports = transactionsRouter;
